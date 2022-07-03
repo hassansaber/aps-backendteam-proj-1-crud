8b43e19cc0248683e05f2979818e486a7cd7d16b
hassansaber <hassansaberkarin@gmail.com>
2022-07-03 20:02:15 +0430

    CRUD done

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -6,9 +6,51 @@ const UserModel = require("../model/user.model");
 // Create
 const createUser = async (req, res) => {
   try {
-    const userReq = new UserModel(req.body);
-    const userResult = await userReq.save();
+    const useNewModel = new UserModel(req.body);
+    const userResult = await useNewModel.save();
     res.status(201).json(userResult);
+  } catch (err) {
+    res.status(500).json(err.message);
+  }
+};
+
+//CRUD
+// Read
+const readUsers = async (req, res) => {
+  try {
+    const usersList = await UserModel.find({});
+    res.status(200).json(usersList);
+  } catch (err) {
+    res.status(500).json(err.message);
+  }
+};
+
+//CRUD
+// Update
+const updateUser = async (req, res) => {
+  try {
+    const updatedUser = await UserModel.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: req.body,
+      },
+      {
+        new: true,
+        runValidators: true,
+      }
+    );
+    res.status(200).json(updatedUser);
+  } catch (err) {
+    res.status(500).json(err.message);
+  }
+};
+
+//CRUD
+// Delete
+const deleteUser = async (req, res) => {
+  try {
+    await UserModel.findByIdAndDelete(req.params.id);
+    res.status(200).json("user has been deleted");
   } catch (err) {
     res.status(500).json(err.message);
   }
@@ -17,4 +59,7 @@ const createUser = async (req, res) => {
 //--
 module.exports = {
   createUser,
+  readUsers,
+  updateUser,
+  deleteUser,
 };
--- routes/user.router.js
+++ routes/user.router.js
@@ -1,15 +1,24 @@
 const router = require("express").Router();
-const { createUser } = require("../controllers/user.controller");
+const {
+  createUser,
+  readUsers,
+  updateUser,
+  deleteUser,
+} = require("../controllers/user.controller");
 
 //--
 
 router.post("/new", createUser);
+// http://localhost:8000/api/v1/user/new
+
+router.get("/", readUsers);
+// http://localhost:8000/api/v1/user/
+
+router.put("/:id", updateUser);
+// http://localhost:8000/api/v1/user/62c09f80bd1b396fdabe26e9
+
+router.delete("/:id", deleteUser);
+// http://localhost:8000/api/v1/user/62c09f80bd1b396fdabe26e9
 
 //--
-
 module.exports = router;
-
-// vaysaaaaa
-// reqire ham doroste?
-// chizi kam nist?
-// check kon
